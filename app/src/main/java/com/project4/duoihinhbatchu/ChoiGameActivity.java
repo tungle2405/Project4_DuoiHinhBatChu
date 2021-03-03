6b431ad35285614fe5724a5bf006dea6ae9a8cb9
tunglxk60 <lexuantung2405@gmail.com>
2021-03-03 20:06:48 +0700

    Them phan hien thi dap an va cau tra loi cho game

--- app/src/main/java/com/project4/duoihinhbatchu/ChoiGameActivity.java
+++ app/src/main/java/com/project4/duoihinhbatchu/ChoiGameActivity.java
@@ -3,12 +3,56 @@ package com.project4.duoihinhbatchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import com.project4.duoihinhbatchu.adapter.DapAnAdapter;
+
+import java.util.ArrayList;
 
 public class ChoiGameActivity extends AppCompatActivity {
+
+    ArrayList<String> arrCauTraLoi;
+    GridView gdvCauTraLoi;
+    ArrayList<String> arrDapAn;
+    GridView gdvDapAn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choi_game);
+        init();
+        anhXa();
+        hienThiCauTraLoi();
+        hienThiDapAn();
+    }
+
+    private void anhXa(){
+        gdvCauTraLoi = findViewById(R.id.gdvCauTraLoi);
+        gdvDapAn = findViewById(R.id.gdvDapAn);
+    }
+    private void init(){
+        arrCauTraLoi = new ArrayList<>();
+        arrCauTraLoi.add("B");
+        arrCauTraLoi.add("B");
+        arrCauTraLoi.add("");
+        arrCauTraLoi.add("B");
+        arrCauTraLoi.add("B");
+
+        arrDapAn = new ArrayList<>();
+        arrDapAn.add("T");
+        arrDapAn.add("U");
+        arrDapAn.add("N");
+        arrDapAn.add("G");
+        arrDapAn.add("P");
+        arrDapAn.add("R");
+        arrDapAn.add("O");
+    }
+    private void hienThiCauTraLoi(){
+        gdvCauTraLoi.setNumColumns(arrCauTraLoi.size());
+        gdvCauTraLoi.setAdapter(new DapAnAdapter(this,0,arrCauTraLoi));
+    }
+    private void hienThiDapAn(){
+        gdvDapAn.setNumColumns(arrDapAn.size()/2);
+        gdvDapAn.setAdapter(new DapAnAdapter(this,0,arrDapAn));
     }
 }
---
+++ app/src/main/java/com/project4/duoihinhbatchu/adapter/DapAnAdapter.java
@@ -0,0 +1,38 @@
+package com.project4.duoihinhbatchu.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.project4.duoihinhbatchu.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DapAnAdapter extends ArrayAdapter<String> {
+    private Context myContext;
+    private ArrayList<String> arr;
+    public DapAnAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+        super(context, resource, objects);
+        this.myContext = context;
+        this.arr = new ArrayList<>(objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater)myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_tra_loi, null);
+        }
+        TextView txvCauTraLoi = convertView.findViewById(R.id.txvCauTraLoi);
+        txvCauTraLoi.setText(this.arr.get(position));
+        return convertView;
+    }
+}
