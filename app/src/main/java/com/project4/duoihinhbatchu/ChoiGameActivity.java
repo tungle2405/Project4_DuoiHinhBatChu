b19b53e9495cb89f54e0c3e2da4b0ce460fcdc31
tunglxk60 <lexuantung2405@gmail.com>
2021-03-13 21:56:33 +0700

    Xet su kien choi game

--- app/src/main/java/com/project4/duoihinhbatchu/ChoiGameActivity.java
+++ app/src/main/java/com/project4/duoihinhbatchu/ChoiGameActivity.java
@@ -3,18 +3,25 @@ package com.project4.duoihinhbatchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.project4.duoihinhbatchu.adapter.DapAnAdapter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ChoiGameActivity extends AppCompatActivity {
+
+    private String dapAn="CaTinh";
 
     ArrayList<String> arrCauTraLoi;
     GridView gdvCauTraLoi;
     ArrayList<String> arrDapAn;
     GridView gdvDapAn;
+
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,9 @@ public class ChoiGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choi_game);
         init();
         anhXa();
+        setOnClick();
+
+        bamData();
         hienThiCauTraLoi();
         hienThiDapAn();
     }
@@ -32,27 +42,71 @@ public class ChoiGameActivity extends AppCompatActivity {
     }
     private void init(){
         arrCauTraLoi = new ArrayList<>();
-        arrCauTraLoi.add("B");
-        arrCauTraLoi.add("B");
-        arrCauTraLoi.add("");
-        arrCauTraLoi.add("B");
-        arrCauTraLoi.add("B");
-
         arrDapAn = new ArrayList<>();
-        arrDapAn.add("T");
-        arrDapAn.add("U");
-        arrDapAn.add("N");
-        arrDapAn.add("G");
-        arrDapAn.add("P");
-        arrDapAn.add("R");
-        arrDapAn.add("O");
     }
     private void hienThiCauTraLoi(){
         gdvCauTraLoi.setNumColumns(arrCauTraLoi.size());
         gdvCauTraLoi.setAdapter(new DapAnAdapter(this,0,arrCauTraLoi));
     }
+    private void setOnClick(){
+        gdvDapAn.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = (String)parent.getItemAtPosition(position);
+                if(s.length()!=0 && index<arrCauTraLoi.size()){
+                    if(arrCauTraLoi.get(index).length()!=0){
+                        return;
+                    }
+                    arrDapAn.set(position,"");
+                    arrCauTraLoi.set(index,s);
+                    index++;
+                    hienThiCauTraLoi();
+                    hienThiDapAn();
+                }
+            }
+        });
+        gdvCauTraLoi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = (String)parent.getItemAtPosition(position);
+                if(s.length() != 0){
+                    index = position;
+                    arrCauTraLoi.set(position,"");
+                    for(int i=0; i<arrDapAn.size();i++){
+                        if(arrDapAn.get(i).length()==0){
+                            arrDapAn.set(i,s);
+                            break;
+                        }
+                    }
+                    hienThiCauTraLoi();
+                    hienThiDapAn();
+                }
+            }
+        });
+    }
     private void hienThiDapAn(){
         gdvDapAn.setNumColumns(arrDapAn.size()/2);
         gdvDapAn.setAdapter(new DapAnAdapter(this,0,arrDapAn));
+    }
+    private void bamData(){
+        arrCauTraLoi.clear();
+        Random r = new Random();
+        for (int i=0;i<dapAn.length();i++){
+            arrCauTraLoi.add("");
+            String s = ""+(char)(r.nextInt(26) + 65);
+            arrDapAn.add(s);
+            String s1 = ""+(char)(r.nextInt(26) + 65);
+            arrDapAn.add(s1);
+        }
+        for (int i=0;i<dapAn.length();i++){
+            String s = ""+dapAn.charAt(i);
+            arrDapAn.set(i,s.toUpperCase());
+        }
+        for (int i=0;i<10;i++){
+            String s = arrDapAn.get(i);
+            int vt = r.nextInt(arrDapAn.size());
+            arrDapAn.set(i,arrDapAn.get(vt));
+            arrDapAn.set(vt,s);
+        }
     }
 }
